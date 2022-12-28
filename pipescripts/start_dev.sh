9aee8e65c5b2673661700784cb25948fae06099a
Aman sharma <aman.sharma2@appinventiv.com>
2022-12-28 15:29:49 +0000

    Update pipescripts/start_dev.sh

--- pipescripts/start_dev.sh
+++ pipescripts/start_dev.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+cd /var/www/html/bodyshop_app/nextjs/dev
 ls -la
 pwd
-tar xzf react-ssr-nextjs_artifact.tar.gz --no-overwrite-dir
-cp env .env
+tar -xzf react-ssr-nextjs_artifact.tar.gz
 rm -rf react-ssr-nextjs_artifact.tar.gz
+cp env .env
 pwd
-#chown -R pythru-user:pythru-user /var/www/html/pythru/dev/node_services/pythru_customer
 ls -la
-#echo "pm2 reload"
 #pm2 reload bodyShop_SSR_React
 pm2 delete bodyShop_SSR_React
-pm2 start ./dist/server.js --name bodyShop_SSR_React
-pm2 save
+pm2 start ./next/server.js --name bodyShop_SSR_React
 #pm2 start npm --name bodyShop_SSR_React -- run start:server
+pm2 status
