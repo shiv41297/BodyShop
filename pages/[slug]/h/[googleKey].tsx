31060190d22c10239dc2ee90ff1680b32bbfcfef
shweta <shweta.saxena@appinventiv.com>
2023-01-06 12:17:21 +0530

    authToken

--- pages/[slug]/h/[googleKey].tsx
+++ pages/[slug]/h/[googleKey].tsx
@@ -15,6 +15,7 @@ export default ProductListingWrapper;
 export const getServerSideProps = wrapper.getServerSideProps((store) =>
   //@ts-ignore-
   async ({ req, res, query, params }) => {
+    let authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOiI2M2EzZWQyYTQ2YWRlMzM4OGRlNjQ4YTkiLCJpc0xvZ2luIjp0cnVlLCJpc0d1ZXN0TG9naW4iOnRydWUsImlhdCI6MTY3MTY4NzQ2NiwiZXhwIjoxNjg3MjM5NDY2fQ.4Eg19HCDEGFUiw562m2nxA7T5WPHZb6bt0yZwfx6Xo0"
 
     console.log(query, params);
     const actionparams = {
@@ -24,9 +25,10 @@ export const getServerSideProps = wrapper.getServerSideProps((store) =>
       query: "",
       // sortBy: "2",
       urlKey: query?.slug,
-      authToken: req.cookies.authToken
+      authToken: authToken
+      // authToken: req.cookies.authToken
     };
-    console.log(actionparams.authToken, "authentication")
+    // console.log(actionparams.authToken, "authentication")
 
     console.log("actionParams", actionparams);
     await store.dispatch(getProductList(actionparams));
