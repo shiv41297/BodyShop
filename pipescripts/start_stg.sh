70bc618aa5b20094a74be7d7a4a07c2f07ced642
shivam chauhan <shivam.chauhan@appinventiv.com>
2023-01-17 09:49:11 +0000

    Update start_stg.sh

--- pipescripts/start_stg.sh
+++ pipescripts/start_stg.sh
@@ -7,6 +7,6 @@ rm -rf react-ssr-nextjs_artifact.tar.gz
 cp env .env
 pwd
 ls -la
-#pm2 reload bodyShop_STG_SSR_Next
-pm2 start npm --name bodyShop_STG_SSR_Next -- run start
+pm2 reload bodyShop_STG_SSR_Next
+#pm2 start npm --name bodyShop_STG_SSR_Next -- run start
 pm2 status
